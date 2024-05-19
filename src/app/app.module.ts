126452a829a3b29dcdd7a5300e7b64bc309d2c31
Éverton Carvalho <evertonsnake@gmail.com>
2024-05-19 00:19:09 -0300

    refactor: boilerplate folders

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigsModule } from './../config/config.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RolesGuard } from '../core/guards/role.guard';
+
+@Module({
+  imports: [ConfigsModule, AuthModule, UsersModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: 'x-version-id',
+  });
+
+  const configService = app.get(ConfigService);
+  if (!configService.get('App.isProduction')) {
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('endpoints list, request and response')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('documentation', app, document);
+  }
+
+  await app.listen(configService.get('APP.port'));
 }
+
 bootstrap();
